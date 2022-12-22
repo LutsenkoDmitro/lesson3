b03bfcd26e9a97ef20bd91d742ecdfb8c4cc8510
LutsenkoDmitro <dmitriylutsenkoo@gmail.com>
2022-12-22 22:34:53 +0200

    New folder for next lesson

--- Lesson3/main.js
+++
@@ -1,6 +0,0 @@
-function completeAndRedirect(){
-    let form = document.forms['form'];
-    let text = document.getElementById("text")
-    text.innerHTML="Введені вами данні: <br> І'мя : "+ form.elements.name.value + "<br> EMAIL : " + form.elements.email.value + "<br> Стать: " + form.elements.gender.value;
-    console.log(form.elements)
-}
